2660c85c412dfef4db25fc83d9a4fa8b8780b7df
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2023-10-04 03:33:46 +0900

    reformat

--- Drill06.py
+++ Drill06.py
@@ -23,7 +23,7 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:  # 마우스 클릭이 있으면
-            points.append((event.x,TUK_HEIGHT - 1 - event.y))   # 클릭된 위치를 새로운 점으로 추가
+            points.append((event.x, TUK_HEIGHT - 1 - event.y))  # 클릭된 위치를 새로운 점으로 추가
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -42,7 +42,7 @@ def reset_world():
     frame = 0
     action = 3
 
-    points = [ ]
+    points = []
     set_new_target_arrow()
 
 
@@ -52,7 +52,7 @@ def set_new_target_arrow():
     global frame
     global target_exists
 
-    if points: # points 리스트 안에 남아있는 점이 있으면
+    if points:  # points 리스트 안에 남아있는 점이 있으면
         sx, sy = cx, cy  # p1 : 시작점
         # hx, hy = 50, 50
         hx, hy = points[0]  # p2 : 끝점
@@ -61,7 +61,7 @@ def set_new_target_arrow():
         frame = 0
         target_exists = True
     else:
-        action = 3 if action == 1 else 2    # 이전에 소년이 우측으로 이동 중이었으면, IDLE 동작 시 우측 바라봄
+        action = 3 if action == 1 else 2  # 이전에 소년이 우측으로 이동 중이었으면, IDLE 동작 시 우측 바라봄
         frame = 0
         target_exists = False
 
@@ -88,9 +88,9 @@ def update_world():
             cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t:t의 비율로 섞은 위치
             cy = (1 - t) * sy + t * hy
             t += 0.001
-        else:   # 목표 지점에 도달하면
+        else:  # 목표 지점에 도달하면
             cx, cy = hx, hy  # 캐릭터 위치를 기존 목적지 위치와 강제로 정확히 일치시킴.
-            del points[0]   # 목표 지점에 왔으므로, 더이상 필요없는 점 삭제
+            del points[0]  # 목표 지점에 왔으므로, 더이상 필요없는 점 삭제
             set_new_target_arrow()
     elif points:  # 목표 지점이 없는 상황에서 새로운 목표 지점이 생기면,
         set_new_target_arrow()
